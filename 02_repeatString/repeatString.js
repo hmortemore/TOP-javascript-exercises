b94b74e90ea5355c716b7cecf612f7fb4cb20ec7
hmortemore <hunter@mortemore.com>
2024-05-03 18:46:36 -0400

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(stringInput, repeatNumber) {
+    if(repeatNumber < 0) {
+        return 'ERROR';
+    }
+    let stringOutput = '';
+    for(i = 0; i < repeatNumber; i++) {
+        stringOutput = stringOutput + stringInput;
+    }
+    return stringOutput;
 };
 
 // Do not edit below this line
