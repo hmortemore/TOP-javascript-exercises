bb3b7d07807e6eb2a21e5888faadf8dfdaf63d2a
hmortemore <hunter@mortemore.com>
2024-05-03 19:46:00 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(startInt, endInt) {
+    let sum = 0;
+    if(typeof startInt != 'number' || typeof endInt != 'number' || startInt < 0 || endInt < 0) {
+        return 'ERROR';
+    }
+    if(startInt > endInt) {
+        let temp = startInt;
+        startInt = endInt;
+        endInt = temp;
+    }
+    for(i = startInt; i <= endInt; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
