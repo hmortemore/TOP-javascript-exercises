d09f4b858b5bfc5b202f83f45fa5a6fd94b78e31
hmortemore <hunter@mortemore.com>
2024-05-03 18:52:53 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    output = '';
+    for(i = word.length - 1; i >= 0; i--) {
+        output = output + word[i];
+    }
+    return output;
 };
 
 // Do not edit below this line
